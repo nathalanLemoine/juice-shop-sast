338ef8e5388c36bccfb269a8475611e2ad05f037
nathalanLemoine <106219@ecole-it.com>
2026-01-13 15:36:39 +0100

    Use environment variable for cookie parser secret

--- server.ts
+++ server.ts
@@ -286,7 +286,8 @@ restoreOverwrittenFilesWithOriginals().then(() => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
   app.use(express.static(path.resolve('frontend/dist/frontend')))
-  app.use(cookieParser('kekse'))
+  const cookieSecret = process.env.COOKIE_SECRET;
+  app.use(cookieParser(cookieSecret));
   // vuln-code-snippet end directoryListingChallenge accessLogDisclosureChallenge
 
   /* Configure and enable backend-side i18n */
